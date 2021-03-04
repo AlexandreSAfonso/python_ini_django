165e3437c7deedb2f48edc60ff936715d9928787
Alexandre Afonso <afonso.s.alexandre@gmail.com>
2021-03-04 15:18:10 -0300

    refactor view to object orientation

--- phone_foward/phone/urls.py
+++ phone_foward/phone/urls.py
@@ -3,7 +3,8 @@ import phone.views as views
 
 
 urlpatterns =[
-    path('', views.index, name='index'),
+    path('', views.Index.as_view(), name='index'),
     path('registered', views.registered_func, name='registered_func'), 
-    path('edit_func/<str:phone_number>', views.edit_func, name='edit_func'),
+    #path('edit_func/<str:phone_number>', views.edit_func, name='edit_func'),
+    path('edit_func/<str:phone_number>', views.EditFunc.as_view(), name='edit_func'),
 ]
--- phone_foward/phone/views.py
+++ phone_foward/phone/views.py
@@ -1,9 +1,15 @@
 from django.shortcuts import render, redirect
 from phone import models
+from django.views import View
 
 # Create your views here.
-def index(request):
-    if request.method == 'POST':
+class Index(View):
+    template_name = 'index.html'
+
+    def get(self, request):
+        return render(request, 'index.html')
+    
+    def post(self, request):
         func = models.Func.create_func(
             phone_number = request.POST['phone_number'],
             alias = request.POST['alias']
@@ -19,7 +25,29 @@ def index(request):
             context ={
                 'error': 'Funcionário não Criado. Tente Novamten :('
             }
-    return render(request, 'index.html')
+        return redirect('registered_func')
+
+
+# def index(request):
+#     if request.method == 'POST':
+#         func = models.Func.create_func(
+#             phone_number = request.POST['phone_number'],
+#             alias = request.POST['alias']
+#         )
+#         context = []
+#         if func is not None:
+#             func.save()
+#             context ={
+#                 'phone_number':request.POST['phone_number'],
+#                 'alias':request.POST['alias']
+#             }
+#         else:
+#             context ={
+#                 'error': 'Funcionário não Criado. Tente Novamten :('
+#             }
+#     return render(request, 'index.html')
+
+
 
 def registered_func(request): 
     all_func = models.Func.objects.all()
@@ -28,13 +56,30 @@ def registered_func(request):
     }
     return render(request, 'registered-func.html', context)
 
-def edit_func(request, phone_number):
-    func = models.Func.objects.get(phone_number=phone_number)
-    if request.method == 'POST':
+class EditFunc(View):
+    template_name = 'edit-func.html'
+    def get(self, request, phone_number):
+        func = models.Func.objects.get(phone_number=phone_number)
+        #request.session['func'] = func
+        return render(request, 'edit-func.html',{
+            'func':func
+        })
+
+    def post(self, request, phone_number):
+        func = models.Func.objects.get(phone_number=phone_number)
         func.phone_number = request.POST['phone_number']
         func.alias = request.POST['alias']
         func.save()
         return redirect('registered_func')
-    return render(request, 'edit-func.html',{
-        'func':func
-    })
+
+
+# def edit_func(request, phone_number):
+#     func = models.Func.objects.get(phone_number=phone_number)
+#     if request.method == 'POST':
+#         func.phone_number = request.POST['phone_number']
+#         func.alias = request.POST['alias']
+#         func.save()
+#         return redirect('registered_func')
+#     return render(request, 'edit-func.html',{
+#         'func':func
+#     })
