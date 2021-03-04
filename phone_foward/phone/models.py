1b88dfe832f04dfaef40a4e666fcf9ca6b6efb29
Alexandre Afonso <afonso.s.alexandre@gmail.com>
2021-03-04 14:41:54 -0300

    Include CRUD edit mode

--- phone_foward/phone/models.py
+++ phone_foward/phone/models.py
@@ -4,3 +4,9 @@ from django.db import models
 class Func(models.Model):
     phone_number = models.CharField(max_length=15, unique=True)
     alias = models.CharField(max_length=40)
+
+    @staticmethod
+    def create_func(phone_number, alias):
+        if '+' not in phone_number:
+            phone_number = '+' + phone_number
+        return Func(phone_number=phone_number, alias=alias)
--- phone_foward/phone/urls.py
+++ phone_foward/phone/urls.py
@@ -5,4 +5,5 @@ import phone.views as views
 urlpatterns =[
     path('', views.index, name='index'),
     path('registered', views.registered_func, name='registered_func'), 
+    path('edit_func/<str:phone_number>', views.edit_func, name='edit_func'),
 ]
