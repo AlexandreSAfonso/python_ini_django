287f6eb8a6e37ffd2698bba1fb73782a81868424
Alexandre Afonso <afonso.s.alexandre@gmail.com>
2021-03-04 12:03:03 -0300

    Create first page test view and return

--- phone_foward/phone/views.py
+++ phone_foward/phone/views.py
@@ -2,4 +2,10 @@ from django.shortcuts import render
 
 # Create your views here.
 def index(request):
+    if request.method == 'POST':
+        context ={
+            'phone_number':request.POST['phone_number'],
+            'alias':request.POST['alias']
+        }
+        return render(request, 'index.html', context)
     return render(request, 'index.html')
